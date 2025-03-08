11d9e2be72bdedf585b1ab17b04b79a11296080c
Gherti <metanigherti0@gmail.com>
2025-03-08 13:23:06 +0100

    Initial Commit

---
+++ NoName/NoNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoNameApp.swift
+//  NoName
+//
+//  Created by Nicoló Metani on 08/03/25.
+//
+
+import SwiftUI
+
+@main
+struct NoNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoNameTests/NoNameTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoNameTests.swift
+//  NoNameTests
+//
+//  Created by Nicoló Metani on 08/03/25.
+//
+
+import Testing
+@testable import NoName
+
+struct NoNameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
