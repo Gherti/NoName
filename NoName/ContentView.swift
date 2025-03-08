1069d046cf6ef063e70fd5b6125429ebb988565f
Gherti <metanigherti0@gmail.com>
2025-03-08 13:37:06 +0100

    chat

--- NoName/ContentView.swift
+++ NoName/ContentView.swift
@@ -9,41 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        ZStack{
             VStack{
-                
-                HStack{
-                    Color(.sRGB, red: 102/255, green: 122/255, blue: 176/255).shadow(radius: 10)
-                }.ignoresSafeArea(.all)
-                    .padding(.bottom,245)
-                
-                
                 ZStack{
-                    
-                    Color(.white)
-                    
-                    VStack{
-                        HStack{
-                        
-                            Image(systemName: "plus.app.fill").frame(alignment: .topTrailing)
-                        }
-                        Circle().strokeBorder(Color.black, lineWidth: 40)
-                    }
-                }
-                
-                ZStack{
-                    Color(.sRGB, red: 102/255, green: 122/255, blue: 176/255).shadow(radius: 15)
+                    Color(.sRGB, red: 102/255, green: 122/255, blue: 176/255).ignoresSafeArea(.all)
                     HStack{
                         Image(systemName: "clock.fill").resizable().frame(width: 37.0, height: 37.0)
+
                         
-                        Image(systemName: "calendar")
-                            .resizable().frame(width: 37.0, height: 37.0).padding(.horizontal, 75)
+                        Image(systemName: "calendar").resizable().frame(width: 37.0, height: 37.0).padding(.horizontal, 75)
                         
                         Image(systemName: "person.fill").resizable().frame(width: 37.0, height: 37.0)
                     }
-                }.ignoresSafeArea(.all)
-                    .padding(.top,200)
-                    
+                }
             }
+            
+        }
             
     }
         
