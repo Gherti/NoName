378116bdb4a17b49ad535bc835b68f60a775e370
Gherti <metanigherti0@gmail.com>
2025-03-12 23:07:36 +0100

    colori con pulsanti

---
+++ NoName/ToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoView.swift
+//  NoName
+//
+//  Created by Nicoló Metani on 12/03/25.
+//
+
+import SwiftUI
+
+struct ToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoView()
+}
