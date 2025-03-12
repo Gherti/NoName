63e69482d21b73440f0df4a10619ad234a66730b
Gherti <metanigherti0@gmail.com>
2025-03-12 13:00:20 +0100

    inseriti i colori

---
+++ NoName/Prove.swift
@@ -0,0 +1,18 @@
+//
+//  Prove.swift
+//  NoName
+//
+//  Created by Nicoló Metani on 12/03/25.
+//
+
+import SwiftUI
+
+struct Prove: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Prove()
+}
