5239b7bf833114930fe9e523dbab55fe1281504e
Gherti <metanigherti0@gmail.com>
2025-03-15 18:04:57 +0100

    zoom aggiunto

---
+++ NoName/AnalogClockView.swift
@@ -0,0 +1,18 @@
+//
+//  AnalogClockView.swift
+//  NoName
+//
+//  Created by Nicoló Metani on 15/03/25.
+//
+
+import SwiftUI
+
+struct AnalogClockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnalogClockView()
+}
---
+++ NoName/ZoomView.swift
@@ -0,0 +1,18 @@
+//
+//  ZoomView.swift
+//  NoName
+//
+//  Created by Nicoló Metani on 15/03/25.
+//
+
+import SwiftUI
+
+struct ZoomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ZoomView()
+}
