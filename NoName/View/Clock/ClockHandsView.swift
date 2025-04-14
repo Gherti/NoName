afc58db29e4fdf071dd80e9a384af7f04de2ae5f
Gherti <metanigherti0@gmail.com>
2025-04-14 12:55:42 +0200

    Aggiustati lag, cambiato il tab bar, cambiato colore icone

---
+++ NoName/View/Clock/ClockHandsView.swift
@@ -0,0 +1,18 @@
+//
+//  ClockHandsView.swift
+//  NoName
+//
+//  Created by Nicoló Metani on 14/04/25.
+//
+
+import SwiftUI
+
+struct ClockHandsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClockHandsView()
+}
